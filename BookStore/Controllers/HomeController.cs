497d8c32b4d0b08e533790b3fe93490febec0db0
AlexChuzha <chuzhaalexander@gmail.com>
2016-10-10 08:24:09 +0400

    First commit

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+using BookStore.Util;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            if (id == 3) {
+                return Redirect("/Home/Index");
+            }
+
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Привет мир!</h2>");
+        }
+
+        public ActionResult GetImage()
+        {
+            string path = "../Images/pussy.jpg";
+            return new ImageResult(path);
+        }
+
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "PDFIcon.pdf";
+            return File(file_path, file_type, file_name);
+        }
+
+        // Отправка массива байтов
+        public FileResult GetBytes()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(mas, file_type, file_name);
+        }
+
+        // Отправка потока
+        public FileResult GetStream()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(fs, file_type, file_name);
+        }
+
+    }
+}
---
+++ BookStore/Controllers/MyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BookStore.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            //string ip = requestContext.HttpContext.Request.UserHostAddress;
+            //var response = requestContext.HttpContext.Response;
+            //response.Write("<h2>Ваш IP-адрес: " + ip + "</h2>");
+
+            string browser = requestContext.HttpContext.Request.Browser.Browser;
+            string user_agent = requestContext.HttpContext.Request.UserAgent;
+            string url = requestContext.HttpContext.Request.RawUrl;
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            string referrer = requestContext.HttpContext.Request.UrlReferrer == null ? "" : requestContext.HttpContext.Request.UrlReferrer.AbsoluteUri;
+            //return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+            //    "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+            var response = requestContext.HttpContext.Response;
+            response.Write("<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>");
+        }
+
+
+
+    }
+}
